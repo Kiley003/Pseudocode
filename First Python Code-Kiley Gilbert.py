a9fcb97d2034ce3ddd17090a2fe08d3fb0e13c63
Kiley003 <KileyFeist@gmail.com>
2024-10-03 19:54:58 -0400

    Add files via upload

---
+++ First Python Code-Kiley Gilbert.py
@@ -0,0 +1,12 @@
+"start"
+print ("Hello World")
+print ("I just wrote my first Python Program")
+print ("This is so much fun")
+"""This code will Prompt for your name set the "name' variable and print message."""
+name=input("Hello, what is your name") #gets the user's name
+print("Welcome, it's so nice to meet you"+name)#prints a message with user's name
+#set variable c to "cats."
+#set variable d to "dogs."
+#set variable s to the sentence to print
+#print the sentence
+print("It's raining cats and dogs")
